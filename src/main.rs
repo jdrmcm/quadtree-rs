2ebdbd0ebb4e51e272012f81c0e13e128226054d
jdrmcm <jdr.mcm@gmail.com>
2024-01-09 23:49:20 -0800

    centered view & other bug fixes

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ use quadtree::*;
 use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let boundary = Rectangle::new(200.0, 200.0, 200.0, 200.0);
-    let mut qt = QuadTree::new(boundary, 4);
     let dim = 400.0;
+    let args: Vec<String> = env::args().collect();
+    let boundary = Rectangle::new(dim / 2.0, dim / 2.0, dim / 2.0, dim / 2.0);
+    let mut qt = QuadTree::new(boundary, 4);
 
     for _ in 0..args[1].parse().expect("ERR: Invalid Number") {
         let p = Point::new(random::<f64>() * dim, random::<f64>() * dim);
--- src/quadtree.rs
+++ src/quadtree.rs
@@ -35,7 +35,6 @@ impl Rectangle {
             h,
         }
     }
-
     fn contains(&self, point: Point) -> bool {
         return point.x > (self.x - self.w) &&
                 point.x < (self.x + self.w) &&
@@ -69,10 +68,8 @@ impl QuadTree {
             divided: false,
         }
     }
-
     fn subdivide(&mut self) {
         let b = &self.boundary;
-
         let ne = Rectangle::new(b.x + b.w/2.0, b.y - b.h / 2.0, b.w/2.0, b.h/2.0);
         self.northeast = Some(Box::new(QuadTree::new(ne, self.capacity)));
         let nw = Rectangle::new(b.x - b.w/2.0, b.y - b.h / 2.0, b.w/2.0, b.h/2.0);
@@ -81,16 +78,12 @@ impl QuadTree {
         self.southeast = Some(Box::new(QuadTree::new(se, self.capacity)));
         let sw = Rectangle::new(b.x - b.w/2.0, b.y + b.h / 2.0, b.w/2.0, b.h/2.0);
         self.southwest = Some(Box::new(QuadTree::new(sw, self.capacity)));
-
         self.divided = true;
     }
-
     pub fn insert(&mut self, point: Point) {
-
         if !self.boundary.contains(point) {
             return;
         }
-
         if self.points.len() < self.capacity {
             self.points.push(point);
         } else {
@@ -103,7 +96,6 @@ impl QuadTree {
             self.southwest.as_mut().unwrap().insert(point);
         }
     }
-
     pub fn show(&self) {
         let serialized = serde_json::to_string(self).unwrap();
         write_data(&serialized);
@@ -124,35 +116,30 @@ fn read_data() -> QuadTree {
 fn view(app: &App, frame: Frame) {
     // get canvas to draw on
     let draw = app.draw();
-
     // set background to blue
     draw.background().color(DARKOLIVEGREEN);
-
     show_quadtree(Box::new(read_data()),  &draw);
-
     // put everything on the frame
     draw.to_frame(app, &frame).unwrap();
 }
 
 fn show_quadtree(qt: Box<QuadTree>, draw: &Draw) {
     draw.rect()
-        .x_y(qt.boundary.x as f32, qt.boundary.y as f32)
+        .x_y(qt.boundary.x as f32 - 200.0, qt.boundary.y as f32 - 200.0)
         .w(qt.boundary.w as f32*2.0)
         .h(qt.boundary.h as f32*2.0)
         .hsv(random(), random(), random());
-
     if qt.divided {
         show_quadtree(qt.northeast.unwrap(), draw);
         show_quadtree(qt.northwest.unwrap(), draw);
         show_quadtree(qt.southeast.unwrap(), draw);
         show_quadtree(qt.southwest.unwrap(), draw);
     }
-
-    for point in qt.points {
+    /*for point in qt.points {
         draw.ellipse()
             .color(WHITE)
             .x_y(point.x as f32, point.y as f32)
-            .w(5.0)
-            .h(5.0);
-    }
+            .w(3.0)
+            .h(3.0);
+    }*/
 }
